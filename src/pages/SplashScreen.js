fb842252012225472ba9b35447186a680acf7258
Richieid23 <fitrarizki23@gmail.com>
2022-01-07 01:48:08 +0700

    first commit

---
+++ src/pages/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const SplashScreen = () => {
+    return (
+        <Text>Spalsh Screen</Text>
+    );
+}
+
+export default SplashScreen;
