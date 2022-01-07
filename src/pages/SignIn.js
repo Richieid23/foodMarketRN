2c79daf0ba6f3edb37b42fb69ad89027c0b92e97
Richieid23 <fitrarizki23@gmail.com>
2022-01-07 18:50:59 +0700

    finish slicing sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Header, TextInput, Button, Gap } from '../components'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Header, TextInput, Button, Gap} from '../components';
 
 const SignIn = ({navigation}) => {
-    return (
-        <View style={styles.page}>
-            <Header title={'Sign In'} subtitle={'Find your best ever meal'}/>
-            <View style={styles.container}>
-                <TextInput label={'Email Address'} placeholder={'Type your email address'}/>
-                <Gap height={16}/>
-                <TextInput label={'Password'} placeholder={'Type your password'}/>
-                <Gap height={24}/>
-                <Button text={'Sign In'}/>
-                <Gap height={12}/>
-                <Button text={'Create new account'} color={'#8092a3'} textColor={'white'} onPress={() => navigation.navigate('SignUp')}/>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.page}>
+      <Header title={'Sign In'} subtitle={'Find your best ever meal'} />
+      <View style={styles.container}>
+        <TextInput
+          label={'Email Address'}
+          placeholder={'Type your email address'}
+        />
+        <Gap height={16} />
+        <TextInput label={'Password'} placeholder={'Type your password'} />
+        <Gap height={24} />
+        <Button text={'Sign In'} />
+        <Gap height={12} />
+        <Button
+          text={'Create new account'}
+          color={'#8092a3'}
+          textColor={'white'}
+          onPress={() => navigation.navigate('SignUp')}
+        />
+      </View>
+    </View>
+  );
+};
 
-export default SignIn
+export default SignIn;
 
 const styles = StyleSheet.create({
-    page: {flex: 1},
-    container: {backgroundColor: 'white', paddingHorizontal: 24, paddingVertical: 26, marginTop: 18, flex: 1}
-})
+  page: {flex: 1},
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingVertical: 26,
+    marginTop: 18,
+    flex: 1,
+  },
+});
