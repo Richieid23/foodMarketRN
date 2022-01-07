c383e4e95e901c1c85f336710e88503801b70564
Richieid23 <fitrarizki23@gmail.com>
2022-01-07 07:28:19 +0700

    add SplashScreen

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {Logo} from '../assets';
 
 const SplashScreen = () => {
-    return (
-        <Text>Spalsh Screen</Text>
-    );
-}
+  return (
+    <View
+      style={{
+        backgroundColor: '#ffc700',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Logo />
+      <View style={{height: 38}} />
+      <Text
+        style={{fontSize: 32, color: '#020202', fontFamily: 'Poppins-Medium'}}>
+        FoodMarket
+      </Text>
+    </View>
+  );
+};
 
 export default SplashScreen;
