38d879307ec818be0718c1e8a3b4e1b9e351ca81
Richieid23 <fitrarizki23@gmail.com>
2022-01-19 09:38:08 +0700

    add tab section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import {ScrollView, View} from 'react-native';
-import {Gap, Header, MenuCard} from '../components';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, StyleSheet, View, Text} from 'react-native';
+import {MenuPic} from '../assets';
+import {Gap, Header, HomeTabSection, MenuCard} from '../components';
 
 const Home = ({navigation}) => {
-  const [menus, setMenus] = useState([]);
+  // const layout = useWindowDimensions();
 
-  const getMenus = async() => {
-      try {
-          const response = await fetch("http://192.168.43.59:5000/api/menu");
-          const jsonData = await response.json();
-          setMenus(jsonData.data.menus);
-      } catch (err) {
-          console.error(err.message);
-      }
-  }
+  // const getMenus = async() => {
+  //     try {
+  //         const response = await fetch("http://192.168.43.59:5000/api/menu");
+  //         const jsonData = await response.json();
+  //         setMenus(jsonData.data.menus);
+  //     } catch (err) {
+  //         console.error(err.message);
+  //     }
+  // }
 
-  useEffect(() => {
-      getMenus();
-  }, []);
+  // useEffect(() => {
+  //     getMenus();
+  // }, []);
 
   return (
-    <View>
+    <View style={styles.page}>
       <Header title={'FoodMarket'} subtitle={'Lets get some foods'} />
-      <ScrollView>
-        <View style={{alignItems: 'center'}}>
-          <Gap height={10} />
-          {menus.map((menu) => {
-            return (
-              <MenuCard key={menu.id} title={menu.name} price={menu.price} onPress={() => navigation.navigate('MenuDetail', menu)} />
-            );
-          })}
-        </View>
-      </ScrollView>
+      <View>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <View style={styles.foodCardContainer}>
+            <Gap width={24} />
+            <MenuCard
+              name={'Nama Menu'}
+              image={MenuPic}
+              rating={4.0}
+              onPress={() => navigation.navigate('MenuDetail')}
+            />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.tabContainer}>
+        <HomeTabSection />
+      </View>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  page: {flex: 1},
+  foodCardContainer: {flexDirection: 'row', marginVertical: 24},
+  tabContainer: {flex: 1},
+});
