38ef749ab5aa11bc224d1da3e6aaf0d3ef0e983a
Richieid23 <fitrarizki23@gmail.com>
2022-01-19 09:32:02 +0700

    add icon & illustration

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -1,7 +1,23 @@
 import IcBack from './ic_back.svg';
+import IcBackWhite from './ic_back_white.svg';
 import IcHomeNormal from './ic_home_normal.svg';
 import IcHome from './ic_home.svg';
 import IcOrderNormal from './ic_order_normal.svg';
 import IcOrder from './ic_order.svg';
+import IcStarOn from './ic_star_on.svg';
+import IcStarOff from './ic_star_off.svg';
+import IcMin from './ic_min.svg';
+import IcPlus from './ic_plus.svg';
 
-export {IcBack, IcHome, IcHomeNormal, IcOrder, IcOrderNormal};
+export {
+  IcBack,
+  IcBackWhite,
+  IcHome,
+  IcHomeNormal,
+  IcOrder,
+  IcOrderNormal,
+  IcStarOff,
+  IcStarOn,
+  IcMin,
+  IcPlus,
+};
--- src/assets/Illustrations/index.js
+++ src/assets/Illustrations/index.js
@@ -1,5 +1,6 @@
 import Logo from './logo.svg';
 import IlSuccessSignUp from './success_signup.svg';
 import IlSuccessOrder from './success_order.svg';
+import IlEmptyOrder from './empty_order.svg';
 
-export {Logo, IlSuccessSignUp, IlSuccessOrder};
+export {Logo, IlSuccessSignUp, IlSuccessOrder, IlEmptyOrder};
