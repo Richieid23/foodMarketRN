ecabf9f6adfcfb74de392730b7a2d69011249936
Richieid23 <fitrarizki23@gmail.com>
2022-01-19 09:35:53 +0700

    add ItemListMenu component

---
+++ src/components/molecules/ItemListMenu.js
@@ -0,0 +1,49 @@
+/* eslint-disable react-native/no-inline-styles */
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Rating} from '.';
+
+const ItemListMenu = ({image, onPress}) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7} onPress={onPress}>
+      <View style={styles.container}>
+        <Image source={image} style={styles.image} />
+        <View style={styles.content}>
+          <Text style={styles.title}>Nama Makanan</Text>
+          <Text style={styles.price}>Harga</Text>
+        </View>
+        <Rating number={4} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ItemListMenu;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+    alignItems: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    overflow: 'hidden',
+    marginRight: 12,
+  },
+  content: {flex: 1},
+  title: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 16,
+    color: '#020202',
+  },
+  price: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 13,
+    color: '#8d92a3',
+  },
+});
