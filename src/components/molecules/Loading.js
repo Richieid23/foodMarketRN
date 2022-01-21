2a8c1947fccb74f904ce491d4d7f6d8197f6416b
Richieid23 <fitrarizki23@gmail.com>
2022-01-21 16:30:05 +0700

    add loading molecules component

---
+++ src/components/molecules/Loading.js
@@ -0,0 +1,31 @@
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#1abc9c" />
+      <Text style={styles.text}>Loading ...</Text>
+    </View>
+  );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: 'Poppins-Regular',
+    marginTop: 12,
+    color: 'white',
+  },
+});
