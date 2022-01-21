5db866da7260e0b90a5bc54d05f0fb778a7b9a64
Richieid23 <fitrarizki23@gmail.com>
2022-01-21 16:33:59 +0700

    add order reducer

---
+++ src/redux/reducer/order.js
@@ -0,0 +1,13 @@
+const initOrder = {
+  orders: [],
+};
+
+export const orderReducer = (state = initOrder, action) => {
+  if (action.type === 'SET_ORDER') {
+    return {
+      ...state,
+      orders: action.value,
+    };
+  }
+  return state;
+};
