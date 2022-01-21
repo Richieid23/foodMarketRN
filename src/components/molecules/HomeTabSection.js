0582babe5e0ff921c4e3af2922ec78b0d1cba104
Richieid23 <fitrarizki23@gmail.com>
2022-01-21 16:28:45 +0700

    redux integration

--- src/components/molecules/HomeTabSection.js
+++ src/components/molecules/HomeTabSection.js
@@ -1,10 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
 import {Dimensions, ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import ItemListMenu from './ItemListMenu';
 import {MenuPic} from '../../assets';
 import {useNavigation} from '@react-navigation/native';
+import {useDispatch, useSelector} from 'react-redux';
+import {getMenuDataByTypes} from '../../redux/action';
 
 const renderTabBar = (props) => (
   <TabBar
@@ -37,29 +39,28 @@ const renderTabBar = (props) => (
 
 const Makanan = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const {makanan} = useSelector((state) => state.homeReducer);
+
+  useEffect(() => {
+    dispatch(getMenuDataByTypes('Makanan'));
+  }, []);
   return (
     <ScrollView>
-      <View style={{paddingTop: 8}}>
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
+      <View style={{paddingTop: 8, paddingHorizontal: 24}}>
+        {makanan.map((item) => {
+          return (
+            <ItemListMenu
+              key={item.id}
+              rating={item.rate}
+              image={item.picture ? item.picture : MenuPic}
+              type={'product'}
+              name={item.name}
+              price={item.price}
+              onPress={() => navigation.navigate('MenuDetail', item)}
+            />
+          );
+        })}
       </View>
     </ScrollView>
   );
@@ -67,30 +68,28 @@ const Makanan = () => {
 
 const Minuman = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const {minuman} = useSelector((state) => state.homeReducer);
 
+  useEffect(() => {
+    dispatch(getMenuDataByTypes('Minuman'));
+  }, []);
   return (
     <ScrollView>
-      <View style={{paddingTop: 8}}>
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
-        <ItemListMenu
-          image={MenuPic}
-          onPress={() => navigation.navigate('MenuDetail')}
-        />
+      <View style={{paddingTop: 8, paddingHorizontal: 24}}>
+        {minuman.map((item) => {
+          return (
+            <ItemListMenu
+              key={item.id}
+              rating={item.rate}
+              image={item.picture ? item.picture : MenuPic}
+              type={'product'}
+              name={item.name}
+              price={item.price}
+              onPress={() => navigation.navigate('MenuDetail', item)}
+            />
+          );
+        })}
       </View>
     </ScrollView>
   );
